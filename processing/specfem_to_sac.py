f406a8c446873e9feba6fd99484a627391b3d96e
williameaton <weaton@princeton.edu>
2025-05-06 16:04:41 -0400

    add processing dir

--- processing/specfem_to_sac.py
+++ processing/specfem_to_sac.py
@@ -1,34 +1,42 @@
-import obspy
+# Converts SPECFEM ascii output to SAC
 from obspy.core.stream import Stream
 from obspy.core.trace import Trace
 import numpy as np
 
+# SPECIFY NETWORKS AND STATIONS:
+# todo: modify to read specfem STATIONS file
 networks = {'MAJO':'IU', 'MDJ': 'IC','INU': 'GG',  'INCN': 'IU', 'NE93':'YP', 'BJT':'IC', 'MA2':'IU', 'XAN':'IC', 'ULN':'IU' }
 stations = ['MAJO', 'INU', 'MDJ', 'INCN', 'NE93', 'BJT', 'MA2', 'XAN', 'ULN']
 
-station = 'MDJ'
+# Will load ascii from ```path/rawdir``` and save sac files in ```path/{chl}grav and``` ```path/{chl}acc```
+fpath = f"/Users/eaton/Documents/Princeton/PEGS_kernels/blobs/test_6_source_cylinder/"
 
-fpath = f"../kernels/adjoint_source/SEPT_5"
 
-rawdir = 'OUTPUT_FILES'
-chl    = 'Z'
+rawdir        = 'raw'   # SPECFEM ascii data directory
+chl           = 'N'     # Channel of interet
+SPECFEM_GLOBE = False   # Set to true for spfm_globe ascii formatting
 
 for grav in [True, False]:
-
     if grav:
         outdir = f'{chl}grav'
     else:
         outdir = f'{chl}acc'
 
 
-    for stn in stations: #[f'{station}']:
+    for stn in stations:
         network = networks[stn]
 
         # Load data and get stats:
         if grav:
-            d = np.loadtxt(f"{fpath}/{rawdir}/{stn}.{network}.MX{chl}C.PGRAV.sem.ascii")
+            if SPECFEM_GLOBE:
+                d = np.loadtxt(f"{fpath}/raw/{network}.{stn}.MX{chl}.C.PGRAV.sem.ascii")
+            else:
+                d = np.loadtxt(f"{fpath}/{rawdir}/{stn}.{network}.MX{chl}C.PGRAV.sem.ascii")
         else:
-            d = np.loadtxt(f"{fpath}/{rawdir}/{stn}.{network}.MX{chl}A.sem.ascii")
+            if SPECFEM_GLOBE:
+                d = np.loadtxt(f"{fpath}/raw/{network}.{stn}.MX{chl}A.sem.ascii")
+            else:
+                d = np.loadtxt(f"{fpath}/{rawdir}/{stn}.{network}.MX{chl}A.sem.ascii")
         dt = np.mean(d[1:, 0] - d[:-1, 0])
         print(dt)
         tr = Trace()
